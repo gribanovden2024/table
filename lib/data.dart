f9af501916f3bdf1126a149e1297020762740973
Denis <gribanovden@gmail.com>
2024-05-13 22:14:48 +0300

    +

--- lib/data.dart
+++ lib/data.dart
@@ -5,40 +5,88 @@ import 'package:collection/collection.dart';
 
 class Data {
   late List<List<dynamic>>? csvTable;
+  late List<Map<String, dynamic>> convertTabled;
 
-  Future<List<String>> readCSV() async {
+  Future<List<Map<String, dynamic>>> getCsv() async {
     final csvString = await rootBundle.loadString('assets/data.csv');
     csvTable = CsvConverter().convert(csvString);
-    List<String> result =
-        csvTable![0].map((value) => value.toString()).toList();
+    convertTabled = convertTable(csvTable!);
+    return convertTabled;
+  }
+
+  List<String> getKeys(List<List<dynamic>>? data) {
+    List<String> result = data![0].map((value) => value.toString()).toList();
     return result.sublist(1);
   }
 
-  List<Map<String, dynamic>> convertList(
-      List<List<dynamic>> inputList, List<String> keys) {
-    List<Map<String, dynamic>> outputList = [];
+  List<Map<String, dynamic>> convertTable(List<List<dynamic>> table) {
+    List<Map<String, dynamic>> convertedTable = [];
 
-    for (var innerList in inputList) {
-      Map<String, dynamic> map = {};
-      innerList.asMap().forEach((index, value) {
-        map[keys[index]] = value;
-      });
-      outputList.add(map);
+    if (table.isNotEmpty) {
+      // Предполагаем, что первая строка содержит заголовки столбцов
+      List<dynamic> headers = table[0];
+
+      // Проходимся по строкам таблицы, начиная с индекса 1,
+      // так как первая строка уже использована как заголовки столбцов
+      for (int i = 1; i < table.length; i++) {
+        Map<String, dynamic> rowMap = {};
+
+        // Проходимся по элементам строки
+        for (int j = 0; j < headers.length; j++) {
+          // Если значение в строке присутствует, добавляем его в Map
+          if (table[i].length > j) {
+            rowMap[headers[j].toString()] = table[i][j];
+          } else {
+            // Если строка не содержит значения для столбца,
+            // присваиваем пустую строку
+            rowMap[headers[j].toString()] = '';
+          }
+        }
+        convertedTable.add(rowMap);
+      }
     }
 
-    return outputList;
+    return convertedTable;
   }
 
-  Map<dynamic, dynamic> groupByKeys(List<String> keys) {
-    // Создаем итоговую карту для хранения результатов группировки
-    var groupedData = <dynamic, dynamic>{};
+  List<Map<String, dynamic>> groupListByKeys(List<Map<String, dynamic>> list, List<String> keys) {
+    // Проверяем, что список и список ключей не пустые
+    if (list.isEmpty || keys.isEmpty) {
+      return [];
+    }
 
-    // Создаем ключ группы, состоящий из значений объекта для указанных ключей
-    var groupKey = keys.map((key) => convertList(csvTable!,keys)[key]).toList();
+    // Создаем карту для хранения сгруппированных данных
+    Map<List<dynamic>, List<Map<String, dynamic>>> groupedData = {};
 
-    // Записываем объект данных в итоговую карту с ключом, сформированным из значений для указанных ключей
-    groupedData[groupKey] = data;
+    // Проходимся по каждому элементу списка
+    for (var item in list) {
+      // Генерируем ключ для группировки, используя значения из элемента и ключи
+      List<dynamic> groupKey = keys.map((key) {
+        return item[key];
+      }).toList();
 
-    return groupedData;
+      // Проверяем, есть ли уже группа с таким ключом
+      if (!groupedData.containsKey(groupKey)) {
+        // Если нет, создаем новую группу
+        groupedData[groupKey] = [];
+      }
+
+      // Добавляем элемент в группу
+      groupedData[groupKey]!.add(item);
+    }
+
+    // Сортируем группы по порядку ключей
+    List<dynamic> sortedGroups = keys.map((key) {
+      return list[0][key];
+    }).toList();
+
+    // Формируем сгруппированный список
+    List<Map<String, dynamic>> groupedList = [];
+    for (var groupKey in sortedGroups) {
+      // Добавляем все элементы из группы в сгруппированный список
+      groupedList.addAll(groupedData[groupKey]!);
+    }
+
+    return groupedList;
   }
 }
--- lib/temp.dart
+++ lib/temp.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'data.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -11,70 +10,80 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   List<String> _firstList = [];
-  final List<String> _secondList = [];
+  List<String> _secondList = [];
+  List<Map<String, dynamic>> groupedData = [];
   final d = Data();
 
   @override
   void initState() {
     super.initState();
-    // d.init();
-    d.readCSV().then((list) => setState(() => _firstList = list));
+    d.getCsv().then((_) {
+      setState(() {
+        _firstList = d.getKeys(d.csvTable);
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-          children: [
-            Expanded(
-              child: ListView.builder(
-                itemCount: _firstList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_firstList[index]),
-                    onTap: () =>
-                        setState(() => _secondList.add(_firstList[index])),
-                  );
-                },
-              ),
+      body: Row(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _firstList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_firstList[index]),
+                  onTap: () => setState(() {
+                    _secondList.add(_firstList[index]);
+                  }),
+                );
+              },
             ),
-            const VerticalDivider(),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _secondList.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(_secondList[index]),
-                    onTap: () =>
-                        setState(() => _secondList.remove(_secondList[index])),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-        bottomNavigationBar: ElevatedButton(
-          onPressed: () => _showBottomSheet(context, d.groupByKeys(_secondList)),
-          style: ButtonStyle(
-            fixedSize: MaterialStateProperty.all(const Size(300, 50)),
-            textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 14)),
           ),
-          child: const Text('Построить'),
-        ));
+          const VerticalDivider(),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _secondList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_secondList[index]),
+                  onTap: () => setState(() {
+                    _secondList.remove(_secondList[index]);
+                  }),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: ElevatedButton(
+        onPressed: () async {
+          groupedData = await d.getCsv();
+          _showBottomSheet(context, d.groupListByKeys(groupedData, _secondList));
+        },
+        style: ButtonStyle(
+          fixedSize: MaterialStateProperty.all(const Size(300, 50)),
+          textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 14)),
+        ),
+        child: const Text('Построить'),
+      ),
+    );
   }
 }
 
 void main() => runApp(const MaterialApp(home: MyHomePage()));
 
-Future<void> _showBottomSheet(context, function) async {
-  String groupedData = await function();
+void _showBottomSheet(BuildContext context, function) {
+  List<Map<String, dynamic>> groupedData = function();
   showModalBottomSheet(
     context: context,
     builder: (BuildContext context) {
       return ListView(
         children: [
           Center(
-            child: TableWidget(groupedData: groupedData),
+            child: DataTableWidget(groupedData),
           ),
         ],
       );
@@ -82,49 +91,35 @@ Future<void> _showBottomSheet(context, function) async {
   );
 }
 
-class TableWidget extends StatelessWidget {
-  final Map<dynamic, dynamic> groupedData;
+class DataTableWidget extends StatelessWidget {
+  final List<Map<String, dynamic>> table;
 
-  TableWidget({required this.groupedData});
+  const DataTableWidget(this.table, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(16.0),
-      decoration: BoxDecoration(
-        border: Border(
-          top: BorderSide(color: Colors.grey),
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            'Table',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 10.0),
-          DataTable(
-            columns: <DataColumn>[
-              DataColumn(label: Text('Key')),
-              DataColumn(label: Text('Value')),
-            ],
-            rows: groupedData.entries
-                .map(
-                  (entry) => DataRow(
-                    cells: <DataCell>[
-                      DataCell(Text(entry.key.toString())),
-                      DataCell(Text(entry.value.toString())),
-                    ],
-                  ),
-                )
-                .toList(),
-          ),
-        ],
-      ),
-    );
+    if (table.isEmpty) {
+      return const Text('Table is empty');
+    }
+
+    List<DataColumn> columns = [];
+    List<DataRow> rows = [];
+
+    // Создаем столбцы таблицы на основе ключей первого элемента
+    for (var key in table.first.keys) {
+      columns.add(DataColumn(label: Text(key)));
+    }
+
+    // Создаем строки таблицы на основе данных
+    for (var row in table) {
+      List<DataCell> cells = [];
+      row.forEach((key, value) {
+        cells.add(DataCell(Text(value.toString())));
+      });
+      rows.add(DataRow(cells: cells));
+    }
+
+    // Возвращаем виджет DataTable с созданными столбцами и строками
+    return DataTable(columns: columns, rows: rows);
   }
 }
